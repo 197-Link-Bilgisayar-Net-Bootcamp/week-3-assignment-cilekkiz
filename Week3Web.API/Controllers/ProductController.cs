66eaecb60fd05934a2add5dcb258f8954c0d05c1
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2022-06-11 02:35:01 +0300

    Product service ve endpoint methodlar eklendi.

---
+++ Week3Web.API/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Week3Web.Service.DTOs;
+using Week3Web.Service.Models;
+using Week3Web.Service.Services;
+
+namespace Week3Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<Response<List<ProductDTO>>> GetAll()
+        {
+            return await _productService.GetAllProductsAsync();
+        }
+        [HttpGet("{categoryId:Guid}")]
+        public async Task<Response<List<ProductDTO>>> GetAllProductsByCategory(Guid categoryId)
+        {
+            return await _productService.GetAllProductsByCategoryAsync(categoryId);
+        }
+        [HttpGet("{Id:Guid}")]
+        public async Task<Response<ProductDTO>> GetProduct(Guid Id)
+        {
+            return await _productService.GetProductAsync(Id);
+        }
+        [HttpPost]
+        public async Task<Response<ProductCreateDTO>> CreateProduct(ProductCreateDTO newEntity)
+        {
+            return await _productService.CreateProductAsync(newEntity);
+        }
+        [HttpPost("{categoryId:Guid}")]
+        public async Task<Response<ProductCreateDTO>> CreateProductByCategory(ProductCreateDTO newEntity, Guid categoryId)
+        {
+            return await _productService.CreateProductByCategoryIdAsync(newEntity, categoryId);
+        }
+        [HttpPut]
+        public async Task<Response<ProductUpdateDTO>> UpdateProduct(ProductUpdateDTO entity)
+        {
+            return await _productService.UpdateProductAsync(entity);
+        }
+        [HttpDelete("{Id:Guid}")]
+        public async Task<Response<ProductDTO>> DeleteProduct(Guid Id)
+        {
+            return await _productService.DeleteProductAsync(Id);
+        }
+    }
+}
---
+++ Week3Web.Service/Services/ProductService.cs
@@ -0,0 +1,217 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Week3Web.Data.Database;
+using Week3Web.Data.Models;
+using Week3Web.Data.Repository;
+using Week3Web.Service.DTOs;
+using Week3Web.Service.Models;
+
+namespace Week3Web.Service.Services
+{
+    public class ProductService
+    {
+        private readonly WebContext _webContext;
+        private readonly GenericRepository<Product> _productRepository;
+        private readonly GenericRepository<Category> _categoryRepository;
+        private readonly GenericRepository<ProductFeature> _productFeatureRepository;
+        private readonly UnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public ProductService(WebContext webContext, GenericRepository<Product> productRepository, GenericRepository<Category> categoryRepository, GenericRepository<ProductFeature> productFeatureRepository, UnitOfWork uow, IMapper mapper)
+        {
+            _webContext = webContext;
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+            _productFeatureRepository = productFeatureRepository;
+            _uow = uow;
+            _mapper = mapper;
+        }
+        public async Task<Response<List<ProductDTO>>> GetAllProductsAsync()
+        {
+            var productList = (await _productRepository.GetAllAsync()).ToList();
+            if (productList.Count == 0)
+            {
+                return new Response<List<ProductDTO>>()
+                {
+                    Data = null,
+                    Errors = new List<string>() { "Urun list empty." },
+                    Status = 404
+                };
+            }
+            var productListModel = _mapper.Map<List<ProductDTO>>(productList);
+            return new Response<List<ProductDTO>>()
+            {
+                Data = productListModel,
+                Errors = null,
+                Status = 200
+            };
+        }
+        public async Task<Response<ProductDTO>> GetProductAsync(Guid Id)
+        {
+            if (Id == Guid.Empty)
+            {
+                return new Response<ProductDTO>()
+                {
+                    Data = null,
+                    Errors = new List<string>() { "parameter not empty."},
+                    Status = 400
+                };
+            }
+            var product = await _productRepository.GetByIdAsync(Id);
+            if (product == null)
+            {
+                return new Response<ProductDTO>()
+                {
+                    Data = null,
+                    Errors = new List<string>() { "product not found." },
+                    Status = 404
+                };
+            }
+            var productModel = _mapper.Map<ProductDTO>(product);
+            return new Response<ProductDTO>()
+            {
+                Data = productModel,
+                Errors = null,
+                Status = 200
+            };
+        }
+        public async Task<Response<List<ProductDTO>>> GetAllProductsByCategoryAsync(Guid categoryId)
+        {
+            if (categoryId == Guid.Empty)
+            {
+                return new Response<List<ProductDTO>>()
+                {
+                    Data = null,
+                    Errors = new List<string>() { "parameter cannot empty." },
+                    Status = 400
+                };
+            }
+            var category = await _categoryRepository.GetByIdAsync(categoryId);
+            if (category == null)
+            {
+                return new Response<List<ProductDTO>>()
+                {
+                    Data = null,
+                    Errors = new List<string>() { "category not found." },
+                    Status = 404
+                };
+            }
+            var productListByCategory = await _webContext.Products.Where(k => k.CategoryId == categoryId).Include(k => k.Category).Include(k => k.ProductFeature).ToListAsync();
+            if (productListByCategory.Count() == 0)
+            {
+                return new Response<List<ProductDTO>>()
+                {
+                    Data = null,
+                    Errors = new List<string>() { "No products found for this category" },
+                    Status = 404
+                };
+            }
+            var productListModel = _mapper.Map<List<ProductDTO>>(productListByCategory);
+            return new Response<List<ProductDTO>>()
+            {
+                Data = productListModel,
+                Errors = null,
+                Status = 200
+            };
+        }
+        public async Task<Response<ProductCreateDTO>> CreateProductAsync(ProductCreateDTO newEntity)
+        {
+            var newProduct = _mapper.Map<Product>(newEntity);
+             await _productRepository.AddAsync(newProduct);
+            await _uow.CommitAsync();
+            return new Response<ProductCreateDTO>()
+            {
+                Data = newEntity,
+                Errors = null,
+                Status = 201
+            };
+        }
+        public async Task<Response<ProductCreateDTO>> CreateProductByCategoryIdAsync(ProductCreateDTO newEntity, Guid categoryId)
+        {
+            if (categoryId == Guid.Empty)
+            {
+                return new Response<ProductCreateDTO>()
+                {
+                    Data = null,
+                    Errors = new List<string>() { "CategoryId not empty." },
+                    Status = 400
+                };
+            }
+            try
+            {
+                var transaction = _uow.BeginTransaction();
+                var category = await _categoryRepository.GetByIdAsync(categoryId);
+                var product = _mapper.Map<Product>(newEntity);
+                await _productRepository.AddAsync(product);
+                await _uow.CommitAsync();
+            }
+            catch (Exception ex)
+            {
+                await _uow.RollBackAsync();
+                throw new Exception($"ex message: {ex.InnerException.Message}");
+            }
+            return new Response<ProductCreateDTO>()
+            {
+                Data = newEntity,
+                Errors = null,
+                Status = 201
+            };
+        }
+        public async Task<Response<ProductUpdateDTO>> UpdateProductAsync(ProductUpdateDTO entity) 
+        {
+            if (entity == null)
+            {
+                return new Response<ProductUpdateDTO>()
+                {
+                    Data = null,
+                    Errors = new List<string>() { "parameter not null." },
+                    Status = 400
+                };
+            }
+            var changedProduct = _mapper.Map<Product>(entity);
+            await _productRepository.UpdateAsync(changedProduct);
+            await _uow.CommitAsync();
+            return new Response<ProductUpdateDTO>()
+            {
+                Data = entity,
+                Errors = null,
+                Status = 200
+            };
+        }
+        public async Task<Response<ProductDTO>> DeleteProductAsync(Guid Id)
+        {
+            if (Id == Guid.Empty)
+            {
+                return new Response<ProductDTO>()
+                {
+                    Data = null,
+                    Errors = new List<string>() { "parameter not empty." },
+                    Status = 400
+                };
+            }
+            var product = await _productRepository.GetByIdAsync(Id);
+            if (product == null)
+            {
+                return new Response<ProductDTO>()
+                {
+                    Data = null,
+                    Errors = new List<string>() { "product not found." },
+                    Status = 404
+                };
+            }
+            await _productRepository.DeleteAsync(product);
+            await _uow.CommitAsync();
+            var productModel = _mapper.Map<ProductDTO>(product);
+            return new Response<ProductDTO>()
+            { 
+                Data = productModel,
+                Errors = null,
+                Status = 200
+            };
+        }
+    }
+}
+
+
+
+
