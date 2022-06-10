a3b2b520a6faffc63671cca19cf77896ae1c3e61
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2022-06-11 02:34:26 +0300

    dto namespace ler duzenlendi.

---
+++ Week3Web.Service/DTOs/ProductByCategoryCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Week3Web.Service.DTOs
+{
+    public class ProductByCategoryCreateDTO
+    {
+        public ProductCreateDTO newProduct { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
--- Week3Web.Service/DTOs/ProductUpdateDTO.cs
+++ Week3Web.Service/DTOs/ProductUpdateDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Week3Web.Data.DTOs
+namespace Week3Web.Service.DTOs
 {
     public class ProductUpdateDTO
     {
